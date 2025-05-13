b90fe26b9eafe4927cf080e1600be74ed536f0b4
Trần Gia Bảo <68107603+zoi5161@users.noreply.github.com>
2025-05-13 13:32:18 +0700

    fix server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import ChatRoutes from '../src/routers/ChatRoutes';
+import ChatRoutes from './routers/chatRoutes';
 import ProductRoutes from './routers/ProductRoutes';
 import BlogRoutes from './routers/BlogRoutes';
 import ImageRoutes from './routers/ImageRoutes';
